d8b5a3830fb3055938ae6e3317064ed9926e0b2d
Barbara Rodrigues <rodriguessbarbara@hotmail.com>
2022-12-19 20:41:25 -0300

    feat: :sparkles: feito a página e estilização de Contato

--- src/components/Contato.js
+++ src/components/Contato.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import Head from './Head/Head';
 import styles from './Contato.module.css'
+import foto from '../img/contato.jpg'
 
 const Contato = () => {
   return (
     <div className={styles.contato}>
       <Head title="PZation | Contato" description="Descrição de contato" />
 
-      <h1>Contato</h1>
+      <img src={foto} alt='máquina de escrever'/>
+      <div>
+        <h1>Entre em contato.</h1>
+        <ul className={styles.lista}>
+          <li>email@dominio.com</li>
+          <li>99999-9999</li>
+          <li>Rua Etc e Tal, 100</li>
+        </ul>
+      </div>
     </div>
   )
 }
